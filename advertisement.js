a41c7b29756f1d55486c93ada9e81303f937f0ec
bavariOP <44003950+bavariOP@users.noreply.github.com>
2022-03-16 22:52:46 +0100

    Update advertisement.js

--- advertisement.js
+++ advertisement.js
@@ -1,1 +1,14 @@
-var adblockJsFile = true;
+window.onload = function(){
+    var button = document.getElementsByName("sandbox")[0]
+    var iframe = document.getElementsByName("framez")[0]
+    button.addEventListener('click',sndbx,false);
+
+    function sndbx(){
+    var nibba = document.getElementById("framez").src;
+    if(iframe.sandbox == 'allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation'){
+    document.getElementById("framez").removeAttribute("sandbox"); 
+    }
+    frames['framez'].location.href=nibba;
+    iframe.sandbox = 'allow-forms allow-pointer-lock allow-same-origin allow-scripts allow-top-navigation';
+    }
+} 
